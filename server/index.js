9cc0650477cc9ebe276a2620221cc215451e37bc
Yash Gupta <gyash1104@gmail.com>
2025-02-09 22:00:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,9 +11,6 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.urlencoded({ extended: true, parameterLimit: 10000, limit: '50mb', }));
 
 //http req recieve
